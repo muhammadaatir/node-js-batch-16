b0984c0eb493510829b6476ea54410e84a124eac
Talal <aatirmemon.8211@gmail.com>
2026-02-19 08:30:37 +0500

    class one

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+app.get("/posts", (req, res) => {
+    res.send([{name: "Hello World", class: "Smit batch 16"}])
+})
+
+app.post("/data", (req, res) => {
+    res.send("Post request done")
+})
+
+app.listen(3000, () => {
+    console.log("Test");
+    console.log("Server is running on 3000");
+})
