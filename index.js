dfb00da36f4b8732c65d2d7330acfc01a3d10e50
muhammadaatir <aatirmemon.8211@gmail.com>
2026-02-24 08:38:11 +0500

    Class 3 code

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import { dataSchema } from "./schema/index.js";
 
 const app = express();
 app.use(express.json());
+app.use("/user-update", (req, res, next) => {
+    console.log("Middleware");
+    if(req.body[0].id){
+        next()
+    }
+    res.status(401).send({message: "user is not authnticated"})
+})
 let data = [];
 console.log("testing");
 
@@ -10,26 +18,38 @@ app.get("/", (req, res) => {
 })
 
 app.get("/users", (req, res) => {
-    res.send(data)
+    try {
+        res.send(data)
+    } catch (err) {
+        res.status(400).send({ error: err })
+    }
 })
 
-app.post("/data", (req, res) => {
+app.post("/data", async (req, res) => {
     try {
-        data.push({...req.body[0], id: Date.now().toString(36)})
-        console.log(Date.now().toString(36));
-        res.send("User Successfully added")
+        await dataSchema.validateAsync(req.body[0])
+        const id = Date.now().toString(36)
+        data.push({ ...req.body[0], id: id })
+        res.status(200).send({ user: { ...req.body[0], id: id }, message: "User Successfully added"})
     } catch (err) {
         console.error(err);
+        res.status(400).send(err.details || "Something went wrong")
     }
 })
 
 app.delete("/user/:id", (req, res) => {
     console.log(req.params.id);
-    const {id} = req.params
+    const { id } = req.params
     data = data.filter(obj => obj.id !== id)
     console.log(data);
-    
-    res.send({message: "User deleted successfully"})
+    res.send({ message: "User deleted successfully" })
+})
+
+app.put('/user-update/:id', (req, res) => {
+    const { id } = req.params;
+    const index = data.findIndex(obj => obj.id === id)
+    data.splice(index, 1, { ...req.body[0], id })
+    res.send({ user: req.body[0], message: "User succesfully updated" })
 })
 
 app.listen(3000, () => {
